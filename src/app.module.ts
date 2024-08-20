b1b154549edf0ced2e6036234d912c62c732f17f
Bruno Felix <brunofelixdesousa@hotmail.com>
2024-08-20 16:16:33 -0300

    Path configurado

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TagModule } from '@app/tag/tag.module';
 
 @Module({
-  imports: [],
+  imports: [TagModule],
   controllers: [],
   providers: [],
 })
