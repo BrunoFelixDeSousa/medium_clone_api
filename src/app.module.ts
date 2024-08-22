3dac49f7e9c8418a7f7b30a047a904fd0f602cfe
Bruno Felix <brunofelixdesousa@hotmail.com>
2024-08-21 23:34:34 -0300

    Refatorado conexão com banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { TagModule } from '@app/tag/tag.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TagEntity } from '@app/tag/tag.entity';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [TagModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      // entities: [__dirname + '../**/*.entity{.ts,.js}'],
+      entities: [TagEntity],
+      synchronize: true,
+    }),
+    TagModule
+  ],
 })
 export class AppModule {}
