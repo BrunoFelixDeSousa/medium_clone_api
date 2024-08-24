6a64646cad643fa450622cedbc3230454f5268c9
Bruno Felix <brunofelixdesousa@hotmail.com>
2024-08-24 19:02:36 -0300

    refactor: retorno do status code alterado para 200 ok

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UsePipes } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, UsePipes } from '@nestjs/common';
 import { AuthService } from '@app/auth/auth.service';
 import { LoginDto, loginSchema } from '@app/auth/dto/login.dto';
 import { ZodValidationPipe } from '@app/pipes/zod-validation-pipe';
@@ -8,6 +8,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+  @HttpCode(HttpStatus.OK)
   @UsePipes(new ZodValidationPipe(loginSchema))
   async login(@Body('user') body: LoginDto) {
     return this.authService.login(body);
