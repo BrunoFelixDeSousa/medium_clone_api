d01db361bff10fc3bd8780ed9781d04e1cf75238
Bruno Felix <brunofelixdesousa@hotmail.com>
2024-08-27 14:56:05 -0300

    refactor: add string aleatória de 6 caracteres alfanuméricos no slug

--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -1,8 +1,12 @@
 export function convertToSlug(title: string): string {
-  return title
-    .toLowerCase()
-    .normalize('NFD')
-    .replace(/[\u0300-\u036f]/g, '')
-    .replace(/[^\w\s-]/g, '')
-    .replace(/\s+/g, '-');
+  return (
+    title
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .replace(/[^\w\s-]/g, '')
+      .replace(/\s+/g, '-') +
+    '-' +
+    ((Math.random() * Math.pow(36, 6)) | 0).toString(36)
+  );
 }
